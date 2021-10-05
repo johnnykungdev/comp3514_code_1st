7582bbed00f9cd20e600b02a06c69ed30177b24f
Johnny Kung <kjohnathan.engi@gmail.com>
2021-10-05 11:16:26 -0700

    first commit

---
+++ Data/Migrations/20211005174748_M1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Code1st.Data.Migrations
+{
+    public partial class M1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
